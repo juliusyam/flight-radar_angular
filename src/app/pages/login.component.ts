059c3c15e93e3bdee72d8ed843c1a52798ce6932
juliusyam <juliusworldwide@gmail.com>
2024-07-23 18:06:21 +0100

    Login Page styling updates

--- src/app/pages/login.component.ts
+++ src/app/pages/login.component.ts
@@ -1,28 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../services/authService';
 import { Router } from '@angular/router';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInput } from '@angular/material/input';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
   template: `
-    <h3>Login Page</h3>
-    <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
-      <label>
-        Email
-        <input type="text" formControlName="email" name="email" />
-      </label>
-      <label>
-        Password
-        <input type="password" formControlName="password" name="password" />
-      </label>
-      <button type="submit" [disabled]="!loginForm.valid">Submit</button>
-    </form>
+    <div style="display: grid; place-items: center; margin-top: 25vh;">
+      <mat-card appearance="outlined" style="max-width: 40rem; width: 100%; ">
+        <mat-card-content>
+          <form [formGroup]="loginForm" (ngSubmit)="onSubmit()" style="display: grid; gap: 1rem;">
+            <mat-form-field appearance="fill">
+              <mat-label>Email</mat-label>
+              <input type="text" placeholder="Email" formControlName="email" name="email" matInput />
+            </mat-form-field>
+            <mat-form-field  appearance="fill">
+              <mat-label>Password</mat-label>
+              <input type="password" placeholder="Password" formControlName="password" name="password" matInput />
+            </mat-form-field>
+            <button mat-button type="submit" [disabled]="!loginForm.valid">Submit</button>
+          </form>
+        </mat-card-content>
+      </mat-card>
+    </div>
   `,
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatFormField,
+    MatLabel,
+    MatButtonModule,
+    MatInput,
+    MatCard,
+    MatCardContent,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPage {
 
